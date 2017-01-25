ae3fda18442e5932ac388e32aea93ad09db3e8fa
Lori A. Burns <lori.burns@gmail.com>
2017-01-25 13:18:07 -0500

    move LAPACK and HDF5 dependencies to targets

---
+++ CMake/FindTargetLAPACK.cmake
@@ -0,0 +1,56 @@
+# FindTargetLAPACK.cmake
+# --------------------
+#
+# LAPACK cmake module to wrap FindLAPACK.cmake in a target.
+#
+# This module sets the following variables in your project: ::
+#
+#   TargetLAPACK_FOUND - true if BLAS/LAPACK found on the system
+#   TargetLAPACK_MESSAGE - status message with BLAS/LAPACK library path list
+#
+## Note that components are passed along to find_package(HDF5 (untested) but not checked in the direct TargetHDF5Config
+#
+# This module *unsets* the following conventional LAPACK variables so as
+#   to force using the target: ::
+#
+#   LAPACK_FOUND
+#   LAPACK_LIBRARIES
+#
+
+set(PN TargetLAPACK)
+
+# 1st precedence - libraries passed in through -DLAPACK_LIBRARIES
+if (LAPACK_LIBRARIES)
+    if (NOT ${PN}_FIND_QUIETLY)
+        message (STATUS "LAPACK detection suppressed.")
+    endif()
+
+    add_library (tgt::lapack INTERFACE IMPORTED)
+    set_property (TARGET tgt::lapack PROPERTY INTERFACE_LINK_LIBRARIES ${LAPACK_LIBRARIES})
+else()
+    # 2nd precedence - target already prepared and findable in TargetLAPACKConfig.cmake
+    find_package (TargetLAPACK QUIET QUIET)
+    if (TARGET tgt::lapack)
+        if (NOT ${PN}_FIND_QUIETLY)
+            message (STATUS "TargetLAPACKConfig detected.")
+        endif()
+    else()
+        # 3rd precedence - usual variables from FindLAPACK.cmake
+        find_package (LAPACK QUIET REQUIRED)
+        if (NOT ${PN}_FIND_QUIETLY)
+            message (STATUS "LAPACK detected.")
+        endif()
+    
+        add_library (tgt::lapack INTERFACE IMPORTED)
+        set_property (TARGET tgt::lapack PROPERTY INTERFACE_LINK_LIBRARIES ${LAPACK_LIBRARIES})
+    endif()
+endif()    
+
+unset (LAPACK_FOUND)
+unset (LAPACK_LIBRARIES)
+
+get_property(_ill TARGET tgt::lapack PROPERTY INTERFACE_LINK_LIBRARIES)
+set(${PN}_MESSAGE "Found LAPACK: ${_ill}")
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(${PN} DEFAULT_MSG ${PN}_MESSAGE)
